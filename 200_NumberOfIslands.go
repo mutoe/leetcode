9cb14852ca665a6fb4f22ff66dbf550080b946c5
mutoe <mutoe@foxmail.com>
2020-04-25 22:34:25 +0800

    200

---
+++ 200_NumberOfIslands.go
@@ -0,0 +1,33 @@
+package main
+
+// DFS Solution
+// 0ms 2.4M
+
+// leetcode submit region begin(Prohibit modification and deletion)
+
+var g [][]byte
+
+func dfs(i, j int) {
+	if i >= 0 && j >= 0 && i < len(g) && j < len(g[0]) && g[i][j] == '1' {
+		g[i][j] = '0'
+		dfs(i-1, j)
+		dfs(i+1, j)
+		dfs(i, j-1)
+		dfs(i, j+1)
+	}
+}
+
+func numIslands(grid [][]byte) (islandsCount int) {
+	g = grid
+	for i, row := range g {
+		for j := range row {
+			if g[i][j] == '1' {
+				islandsCount++
+				dfs(i, j)
+			}
+		}
+	}
+	return
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
---
+++ 200_NumberOfIslands_test.go
@@ -0,0 +1,65 @@
+package main
+
+import "testing"
+
+func Test_numIslands(t *testing.T) {
+	type args struct {
+		grid [][]byte
+	}
+	tests := []struct {
+		name             string
+		args             args
+		wantIslandsCount int
+	}{
+		{
+			name: "test1",
+			args: args{grid: [][]byte{
+				{'1', '1', '1', '1', '0'},
+				{'1', '1', '0', '1', '0'},
+				{'1', '1', '0', '0', '0'},
+				{'0', '0', '0', '0', '0'},
+			}},
+			wantIslandsCount: 1,
+		},
+		{
+			name: "test2",
+			args: args{grid: [][]byte{
+				{'1', '1', '0', '0', '0'},
+				{'1', '1', '0', '0', '0'},
+				{'0', '0', '1', '0', '0'},
+				{'0', '0', '0', '1', '1'},
+			}},
+			wantIslandsCount: 3,
+		},
+		{
+			name:             "test3",
+			args:             args{grid: [][]byte{}},
+			wantIslandsCount: 0,
+		},
+		{
+			name: "test4",
+			args: args{grid: [][]byte{
+				{'1', '1', '1'},
+				{'0', '1', '0'},
+				{'1', '1', '1'},
+			}},
+			wantIslandsCount: 1,
+		},
+		{
+			name: "test5",
+			args: args{grid: [][]byte{
+				{'1', '0', '1'},
+				{'1', '1', '1'},
+				{'1', '0', '1'},
+			}},
+			wantIslandsCount: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotIslandsCount := numIslands(tt.args.grid); gotIslandsCount != tt.wantIslandsCount {
+				t.Errorf("numIslands() = %v, want %v", gotIslandsCount, tt.wantIslandsCount)
+			}
+		})
+	}
+}
