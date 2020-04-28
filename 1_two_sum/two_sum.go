1d8c2e9ebfba81af427611e1369f16e7dba7113a
mutoe <mutoe@foxmail.com>
2020-04-29 01:05:06 +0800

    1 36ms 30.28%

---
+++ 1_two_sum/two_sum.go
@@ -0,0 +1,20 @@
+package two_sum
+
+// https://leetcode.com/problems/two-sum
+
+// time: O(n*log(n)) 36ms 30.28%
+// space: O(1) 2.9M
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
---
+++ 1_two_sum/two_sum_test.go
@@ -0,0 +1,36 @@
+package two_sum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_twoSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "test1",
+			args: args{[]int{2, 7, 11, 15}, 9},
+			want: []int{0, 1},
+		},
+		{
+			name: "test1",
+			args: args{[]int{3, 2, 4}, 6},
+			want: []int{1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
