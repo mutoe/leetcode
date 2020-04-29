01f72cea57565eae990143f61e965653400f5458
mutoe <mutoe@foxmail.com>
2020-04-30 00:56:55 +0800

    202 0ms 100%

---
+++ 202_happy_number/happy_number.go
@@ -0,0 +1,31 @@
+package happy_number
+
+// https://leetcode.com/problems/happy-number
+
+// time: O(1) 0ms 100
+// space: O(1) 2.1M
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func sumOfSquares(n int) (ret int) {
+	for n > 0 {
+		remainder := n % 10
+		ret += remainder * remainder
+		n /= 10
+	}
+	return
+}
+
+func isHappy(n int) bool {
+	numsMap := make(map[int]bool)
+
+	for n != 1 {
+		if numsMap[n] == true {
+			return false
+		}
+		numsMap[n] = true
+		n = sumOfSquares(n)
+	}
+	return true
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
---
+++ 202_happy_number/happy_number_test.go
@@ -0,0 +1,27 @@
+package happy_number
+
+import "testing"
+
+func Test_isHappy(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test",
+			args: args{19},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isHappy(tt.args.n); got != tt.want {
+				t.Errorf("isHappy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
