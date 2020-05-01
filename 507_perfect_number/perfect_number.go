fdf2bf9f84ff16b8c2c44f46a2a1bacd22c9d128
mutoe <mutoe@foxmail.com>
2020-05-01 15:55:01 +0800

    507 0ms 100%

--- 507_perfect_number/perfect_number.go
+++ 507_perfect_number/perfect_number.go
@@ -1,8 +1,10 @@
 package perfect_number
 
+import "math"
+
 // https://leetcode.com/problems/perfect-number
 
-// time: O(log(n)) 1124ms 40.38%
+// time: O(log(n)) 0ms 100%
 // space: O(1) 2M
 
 // leetcode submit region begin(Prohibit modification and deletion)
@@ -10,12 +12,11 @@ func checkPerfectNumber(num int) bool {
 	if num < 6 {
 		return false
 	}
-	largestDivisor := num / 2
+	sqrtNum := int(math.Sqrt(float64(num)))
 	divisorSum := 1
-	for i := 2; i < largestDivisor; i++ {
+	for i := 2; i <= sqrtNum; i++ {
 		if num%i == 0 {
 			divisorSum += i + num/i
-			largestDivisor = num / i
 		}
 	}
 	return divisorSum == num
