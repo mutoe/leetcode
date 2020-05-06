87de5826ae3b97d40c1f7a6edecb9efb084a6322
mutoe <mutoe@foxmail.com>
2020-05-06 13:54:38 +0800

    11 296ms 26.72%

---
+++ 11_container_with_most_water/container_with_most_water.go
@@ -0,0 +1,28 @@
+package container_with_most_water
+
+// https://leetcode.com/problems/container-with-most-water
+
+// time: O(n^2) 296ms 26.72%
+// space: O(1) 5.8M
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func maxArea(height []int) (ret int) {
+	size := len(height)
+	for i := 0; i < size; i++ {
+		for j := i + 1; j < size; j++ {
+			if area := (j - i) * min(height[i], height[j]); area > ret {
+				ret = area
+			}
+		}
+	}
+	return
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
