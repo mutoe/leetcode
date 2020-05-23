2531c51a05e530f81734eb9b6da340c665177b8c
mutoe <mutoe@foxmail.com>
2020-05-23 19:35:14 +0800

    6 8ms 74.58%

--- 6_zigzag_conversion/zigzag_conversion.go
+++ 6_zigzag_conversion/zigzag_conversion.go
@@ -2,28 +2,30 @@ package zigzag_conversion
 
 // https://leetcode.com/problems/zigzag-conversion
 
-// time: O(n) 12ms 51.98%
-// space: O(n) 6.4M
+// time: O(n) 8ms 74.58%
+// space: O(n) 5.8M
 
 // leetcode submit region begin(Prohibit modification and deletion)
-func convert(s string, numRows int) (ret string) {
+func convert(s string, numRows int) string {
 	if numRows == 1 {
 		return s
 	}
-	rows := make([]string, numRows)
-	for i, sign := 0, 1; len(s) > 0; i += sign {
-		rows[i] += string(s[0])
-		s = s[1:]
+	rows := make([][]byte, numRows)
+	for si, i, sign := 0, 0, 1; si < len(s); {
+		rows[i] = append(rows[i], s[si])
 		if sign > 0 && i >= numRows-1 {
 			sign = -1
 		} else if sign < 0 && i <= 0 {
 			sign = 1
 		}
+		si++
+		i += sign
 	}
+	result := make([]byte, 0, len(s))
 	for _, row := range rows {
-		ret += row
+		result = append(result, row...)
 	}
-	return
+	return string(result)
 }
 
 // leetcode submit region end(Prohibit modification and deletion)
