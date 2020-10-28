33a1d3e1cef407ed3de9030a245f0aea0e5d7d34
mutoe <mutoe@foxmail.com>
2020-10-29 00:16:58 +0800

    468 0ms 100%

---
+++ 468_validate_ip_address/validate_ip_address.go
@@ -0,0 +1,79 @@
+package validate_ip_address
+
+import (
+	"strconv"
+	"strings"
+)
+
+// https://leetcode.com/problems/validate-ip-address
+
+// level: 2
+// time: O(1) 0ms 100%
+// space: O(1) 2M 6.67%
+
+// leetcode submit region begin(Prohibit modification and deletion)
+
+func isValidPartOfIpv4(str string) bool {
+	if str == "" || len(str) > 3 {
+		return false
+	}
+	for i, s := range str {
+		if i == 0 && len(str) != 1 && s == '0' {
+			return false
+		}
+		if s >= '0' && s <= '9' {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func isValidPartOfIpv6(str string) bool {
+	if len(str) < 1 || len(str) > 4 {
+		return false
+	}
+	for _, s := range str {
+		if s >= '0' && s <= '9' {
+			continue
+		} else if s >= 'A' && s <= 'F' {
+			continue
+		} else if s >= 'a' && s <= 'f' {
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+func validIPAddress(IP string) string {
+	s := strings.Split(IP, ".")
+	if len(s) == 4 {
+		for _, s2 := range s {
+			if !isValidPartOfIpv4(s2) {
+				return "Neither"
+			}
+			num, _ := strconv.Atoi(s2)
+			if num > 255 {
+				return "Neither"
+			}
+		}
+		return "IPv4"
+	} else if len(s) != 1 {
+		return "Neither"
+	}
+
+	s = strings.Split(IP, ":")
+	if len(s) != 8 {
+		return "Neither"
+	}
+	for _, s2 := range s {
+		if !isValidPartOfIpv6(s2) {
+			return "Neither"
+		}
+	}
+	return "IPv6"
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
---
+++ 468_validate_ip_address/validate_ip_address_test.go
@@ -0,0 +1,82 @@
+package validate_ip_address
+
+import "testing"
+
+func Test_validIPAddress(t *testing.T) {
+	type args struct {
+		IP string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "172.16.254.1",
+			args: args{"172.16.254.1"},
+			want: "IPv4",
+		},
+		{
+			name: "01.01.01.01",
+			args: args{"01.01.01.01"},
+			want: "Neither",
+		},
+		{
+			name: "192.168.1.0",
+			args: args{"192.168.1.0"},
+			want: "IPv4",
+		},
+		{
+			name: "1.0.1.",
+			args: args{"1.0.1."},
+			want: "Neither",
+		},
+		{
+			name: "2001:0db8:85a3:0:0:8A2E:0370:7334",
+			args: args{"2001:0db8:85a3:0:0:8A2E:0370:7334"},
+			want: "IPv6",
+		},
+		{
+			name: "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+			args: args{"2001:0db8:85a3:0000:0000:8a2e:0370:7334"},
+			want: "IPv6",
+		},
+		{
+			name: "256.256.256.256",
+			args: args{"256.256.256.256"},
+			want: "Neither",
+		},
+		{
+			name: "2001:db8:85a3:0::8a2E:0370:7334",
+			args: args{"2001:db8:85a3:0::8a2E:0370:7334"},
+			want: "Neither",
+		},
+		{
+			name: "2001:0db8:85a3:0:0:8A2E:0370:7334:",
+			args: args{"2001:0db8:85a3:0:0:8A2E:0370:7334:"},
+			want: "Neither",
+		},
+		{
+			name: "1e1.4.5.6",
+			args: args{"1e1.4.5.6"},
+			want: "Neither",
+		},
+		{
+			name: "2001:0db8:85a3::8A2E:037j:7334:",
+			args: args{"2001:0db8:85a3::8A2E:037j:7334:"},
+			want: "Neither",
+		},
+		{
+			name: "02001:0db8:85a3:0000:0000:8a2e:0370:7334",
+			args: args{"02001:0db8:85a3:0000:0000:8a2e:0370:7334"},
+			want: "Neither",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validIPAddress(tt.args.IP); got != tt.want {
+				t.Errorf("validIPAddress() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
