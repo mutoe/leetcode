dbe656f69a3e4eb55e24bf4fa8c666e017fdb05b
mutoe <mutoe@foxmail.com>
2020-10-29 00:29:51 +0800

    468 0ms 100%

--- 468_validate_ip_address/validate_ip_address_test.go
+++ 468_validate_ip_address/validate_ip_address_test.go
@@ -67,6 +67,11 @@ func Test_validIPAddress(t *testing.T) {
 			want: "Neither",
 		},
 		{
+			name: "2001:0db8:85a3::8A2E:037j:7334:",
+			args: args{"2001:0db8:85a3:0:8A2E:037j:7334:0"},
+			want: "Neither",
+		},
+		{
 			name: "02001:0db8:85a3:0000:0000:8a2e:0370:7334",
 			args: args{"02001:0db8:85a3:0000:0000:8a2e:0370:7334"},
 			want: "Neither",
