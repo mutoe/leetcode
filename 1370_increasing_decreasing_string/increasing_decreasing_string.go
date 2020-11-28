65e65ec36941cb89815b7aaf8e4f22f683982632
mutoe <mutoe@foxmail.com>
2020-11-28 22:07:33 +0800

    1370 0ms 100%

--- 1370_increasing_decreasing_string/increasing_decreasing_string.go
+++ 1370_increasing_decreasing_string/increasing_decreasing_string.go
@@ -1,42 +1,33 @@
 package increasing_decreasing_string
 
-import "sort"
-
 // https://leetcode.com/problems/increasing-decreasing-string
 
 // level: 1
-// time: O(n*log(n)) 12ms 30.77%
-// space: O(n) 3.3M 64.10%
+// time: O(n) 0ms 100%
+// space: O(n) 3.4M 56.41%
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func sortString(s string) string {
-	bytes := []byte(s)
+	var chars [26]byte
 	ret := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		chars[s[i]-'a']++
+	}
 
-	sort.Slice(bytes, func(i, j int) bool {
-		return bytes[i] < bytes[j]
-	})
 	for len(ret) < len(s) {
 		i := 0
-		var lastChar byte
-		for i < len(bytes) {
-			if lastChar != bytes[i] {
-				ret = append(ret, bytes[i])
-				lastChar = bytes[i]
-				bytes = append(bytes[:i], bytes[i+1:]...)
-			} else {
-				i++
+		for ; i < 26; i++ {
+			if chars[i] > 0 {
+				ret = append(ret, byte(i)+'a')
+				chars[i]--
 			}
 		}
 		i--
-		lastChar = 0
-		for i >= 0 {
-			if lastChar != bytes[i] {
-				ret = append(ret, bytes[i])
-				lastChar = bytes[i]
-				bytes = append(bytes[:i], bytes[i+1:]...)
+		for ; i >= 0; i-- {
+			if chars[i] > 0 {
+				ret = append(ret, byte(i)+'a')
+				chars[i]--
 			}
-			i--
 		}
 	}
 
