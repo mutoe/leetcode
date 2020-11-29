fb909abfb5ec6f6eebb7042f83a23db99c893475
mutoe <mutoe@foxmail.com>
2020-11-30 01:27:20 +0800

    496 0ms 100% optimize using map

--- 496_next_greater_element_i/next_greater_element_i.go
+++ 496_next_greater_element_i/next_greater_element_i.go
@@ -3,13 +3,15 @@ package next_greater_element_i
 // https://leetcode.com/problems/next-greater-element-i
 
 // level: 1
-// time: O(n^2) 4ms 86.21%
-// space: O(n) 2.8M 91.95%
+// time: O(n^2) 0ms 100%
+// space: O(n) 3.4M 60.92%
+
+// TODO: Optimize to O(n) (using Stack)
 
 // leetcode submit region begin(Prohibit modification and deletion)
-func find(nums []int, target int) int {
+func findNext(nums []int, target int) int {
 	for i := 0; i < len(nums); i++ {
-		if nums[i] == target {
+		if nums[i] > target {
 			return i
 		}
 	}
@@ -18,18 +20,19 @@ func find(nums []int, target int) int {
 
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
 	ret := make([]int, len(nums1))
-Outer:
+	greaterMap := map[int]int{}
+
+	for i := 0; i < len(nums2); i++ {
+		greaterMap[nums2[i]] = i + 1
+	}
+
 	for i := 0; i < len(nums1); i++ {
-		targetIndex := find(nums2, nums1[i])
-		if targetIndex < 0 {
-			ret[i] = -1
-			continue
-		}
-		rest := nums2[targetIndex+1:]
-		for j := 0; j < len(rest); j++ {
-			if nums2[j+targetIndex+1] > nums2[targetIndex] {
-				ret[i] = nums2[j+targetIndex+1]
-				continue Outer
+		index := greaterMap[nums1[i]]
+		if index > 0 {
+			el := findNext(nums2[index:], nums1[i])
+			if el >= 0 {
+				ret[i] = nums2[el+index]
+				continue
 			}
 		}
 		ret[i] = -1
