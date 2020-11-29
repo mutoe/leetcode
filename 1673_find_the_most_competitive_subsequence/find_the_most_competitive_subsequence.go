bd614a5784c322c639caa496a90725942a77e95a
mutoe <mutoe@foxmail.com>
2020-11-29 12:28:18 +0800

    1673 180ms

---
+++ 1673_find_the_most_competitive_subsequence/find_the_most_competitive_subsequence.go
@@ -0,0 +1,39 @@
+package find_the_most_competitive_subsequence
+
+import "sort"
+
+// https://leetcode.com/problems/shuffle-string
+
+// level: 1
+// time: O(n^2) 180ms
+// space: O(n) 10.1M
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func mostCompetitive(nums []int, k int) []int {
+	var ret []int
+	l, r := 0, len(nums)-k
+
+	if sort.IntsAreSorted(nums) {
+		return nums[0:k]
+	}
+
+	for len(ret) < k && r < len(nums) {
+		minIndex := min(nums[l : r+1])
+		ret = append(ret, nums[l+minIndex])
+		l += minIndex + 1
+		r++
+	}
+	return ret
+}
+
+func min(nums []int) int {
+	min := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] < nums[min] {
+			min = i
+		}
+	}
+	return min
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
