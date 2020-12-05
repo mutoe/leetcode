f4bf3e8fd7312bdbccdbcf60b462d082266275fb
mutoe <mutoe@foxmail.com>
2020-12-05 17:10:41 +0800

    141 4ms 96%

--- 141_linked_list_cycle/linked_list_cycle.go
+++ 141_linked_list_cycle/linked_list_cycle.go
@@ -3,14 +3,14 @@ package linked_list_cycle
 // https://leetcode.com/problems/linked-list-cycle
 
 // level: 1
-// time: O(n) 8ms 22.44%
-// space: O(n) 5.2M 16.22%
+// time: O(n) 4ms 96%
+// space: O(1) 3.8M 24.67%
 
 import (
 	. "../utils/list_node"
 )
 
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -19,16 +19,15 @@ import (
  * }
  */
 func hasCycle(head *ListNode) bool {
-	m := make(map[*ListNode]bool)
-	cur := head
-	for cur != nil {
-		if m[cur] {
-			return true
-		}
-		m[cur] = true
-		cur = cur.Next
+	if head == nil {
+		return false
 	}
-	return false
+	for slow, fast := head, head.Next; slow != fast; slow, fast = slow.Next, fast.Next.Next {
+		if slow == nil || fast == nil || fast.Next == nil {
+			return false
+		}
+	}
+	return true
 }
 
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
--- 141_linked_list_cycle/linked_list_cycle_test.go
+++ 141_linked_list_cycle/linked_list_cycle_test.go
@@ -28,7 +28,12 @@ func Test_hasCycle(t *testing.T) {
 		},
 		{
 			name: "test3",
-			args: args{[]int{1}, 1},
+			args: args{[]int{1}, -1},
+			want: false,
+		},
+		{
+			name: "test4",
+			args: args{[]int{1, 3}, -1},
 			want: false,
 		},
 	}
@@ -41,21 +46,3 @@ func Test_hasCycle(t *testing.T) {
 		})
 	}
 }
-
-/*
-Input: head = [3,2,0,-4], pos = 1
-Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
-
-Example 2:
-
-Input: head = [1,2], pos = 0
-Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
-
-Example 3:
-
-Input: head = [1], pos = -1
-Output: false
-Explanation: There is no cycle in the linked list.
-*/
