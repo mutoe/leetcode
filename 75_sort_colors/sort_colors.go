a156127b70168c3bd060adb180d58c25f5004768
mutoe <mutoe@foxmail.com>
2020-12-10 01:01:29 +0800

    75 0ms 100% (three-pointer)

--- 75_sort_colors/sort_colors.go
+++ 75_sort_colors/sort_colors.go
@@ -4,21 +4,26 @@ package sort_colors
 
 // level: 2
 // time: O(n) 0ms 100%
-// space: O(1) 2.1M 64.29%
+// space: O(1) 2.0M 64.29%
+
+// One-pass solution (three-pointer)
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func sortColors(nums []int) {
-	colors := [3]int{}
-	for _, num := range nums {
-		colors[num]++
-	}
-	k := 0
-	for n, count := range colors {
-		for i := k; i < k+count; i++ {
-			nums[i] = n
+	zero, i, two := -1, 0, len(nums)
+	for i < two {
+		if nums[i] == 1 {
+			i++
+		} else if nums[i] == 0 {
+			zero++
+			nums[i], nums[zero] = nums[zero], nums[i]
+			i++
+		} else {
+			two--
+			nums[i], nums[two] = nums[two], nums[i]
 		}
-		k += count
 	}
+
 }
 
 // leetcode submit region end(Prohibit modification and deletion)
--- 75_sort_colors/sort_colors_test.go
+++ 75_sort_colors/sort_colors_test.go
@@ -39,6 +39,31 @@ func Test_sortColors(t *testing.T) {
 			args: args{[]int{2, 1, 2, 0, 1, 1, 1, 1}},
 			want: []int{0, 1, 1, 1, 1, 1, 2, 2},
 		},
+		{
+			name: "test6",
+			args: args{[]int{2, 0, 2, 1, 1, 0}},
+			want: []int{0, 0, 1, 1, 2, 2},
+		},
+		{
+			name: "test7",
+			args: args{[]int{0, 0}},
+			want: []int{0, 0},
+		},
+		{
+			name: "test8",
+			args: args{[]int{1, 1}},
+			want: []int{1, 1},
+		},
+		{
+			name: "test9",
+			args: args{[]int{2, 2}},
+			want: []int{2, 2},
+		},
+		{
+			name: "test10",
+			args: args{[]int{1, 2, 0}},
+			want: []int{0, 1, 2},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
