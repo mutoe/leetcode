ced99d32854b65138551bb091250eff9cfb40492
mutoe <mutoe@foxmail.com>
2020-12-11 00:09:19 +0800

    704 36ms 67.18%

--- 704_binary_search/binary_search.go
+++ 704_binary_search/binary_search.go
@@ -4,25 +4,24 @@ package binary_search
 
 // level: 1
 // time: O(log(n)) 36ms 67.19%
-// space: O(log(n)) 6.5M 81.82%
+// space: O(1) 6.5M 81.82%
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func search(nums []int, target int) int {
-	return binary(nums, 0, len(nums)-1, target)
-}
-
-func binary(nums []int, l, r, target int) int {
-	if l > r {
-		return -1
+	l, r := 0, len(nums)-1
+	var mid int
+	for l <= r {
+		mid = l + (r-l)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
 	}
-	mid := l + (r-l)>>1
-	if nums[mid] == target {
-		return mid
-	} else if nums[mid] > target {
-		return binary(nums, l, mid-1, target)
-	} else {
-		return binary(nums, mid+1, r, target)
-	}
+	return -1
 }
 
 // leetcode submit region end(Prohibit modification and deletion)
