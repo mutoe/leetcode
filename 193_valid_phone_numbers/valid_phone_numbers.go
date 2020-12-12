3bc757887b76a1e5cb509560d635b3f3b7a82b49
mutoe <mutoe@foxmail.com>
2020-12-13 02:05:15 +0800

    193 0ms 100%

---
+++ 193_valid_phone_numbers/valid_phone_numbers.go
@@ -0,0 +1,9 @@
+package valid_phone_numbers
+
+// https://leetcode.com/problems/valid-phone-numbers
+
+// level: 1
+// time: O(n) 0ms 100%
+// space: O(1) 3.9M 6.02%
+
+// There is no code of Go type for this problem
