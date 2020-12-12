dcfba297ae447b48a1630585d5d5137dc20fe3c2
mutoe <mutoe@foxmail.com>
2020-12-13 02:12:18 +0800

    195 0ms 100%

---
+++ 195_tenth_line/tenth_line.bash
@@ -0,0 +1,3 @@
+sed -n '10p' file.txt
+
+awk 'NR == 10' file.txt
---
+++ 195_tenth_line/tenth_line.go
@@ -0,0 +1,9 @@
+package tenth_line
+
+// https://leetcode.com/problems/tenth-line
+
+// level: 1
+// time: O(1) 0ms 100%
+// space: O(1) 3.5M 80.19%
+
+// There is no code of Go type for this problem
