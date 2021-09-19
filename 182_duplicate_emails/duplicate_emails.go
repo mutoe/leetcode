74973d46fb4d4c356aaa5608ccbf61f72080084b
mutoe <mutoe@foxmail.com>
2021-09-20 01:04:52 +0800

    182 343ms 42.84%

---
+++ 182_duplicate_emails/duplicate_emails.go
@@ -0,0 +1,14 @@
+package duplicate_emails
+
+// https://leetcode.com/problems/duplicate-emails
+
+// level: 1
+// time: unknown 343ms 42.48%
+// space: unknown 0M 100%
+
+//select Email
+//from Person
+//group by Email
+//having count(Email) > 1;
+
+//There is no code of Go type for this problem
