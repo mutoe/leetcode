0a00c1414ec25b40b4f7ae298f257eddc364947e
mutoe <mutoe@foxmail.com>
2021-09-20 16:25:51 +0800

    387 4ms 97.53%

--- 387_first_unique_character_in_a_string/first_unique_character_in_a_string.go
+++ 387_first_unique_character_in_a_string/first_unique_character_in_a_string.go
@@ -1,33 +1,23 @@
 package first_unique_character_in_a_string
 
-import "math"
-
 // https://leetcode.com/problems/first-unique-character-in-a-string
 
 // level: 1
-// time: O(n) 9ms 65.65%
-// space: O(1) 5.7M 33.02%
+// time: O(n) 4ms 97.53%
+// space: O(1) 5.5M 43.83%
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func firstUniqChar(s string) int {
 	bucket := [26]int{}
 	for i := 0; i < len(s); i++ {
-		if bucket[s[i]-'a'] == 0 {
-			bucket[s[i]-'a'] = i + 1
-		} else {
-			bucket[s[i]-'a'] = -1
+		bucket[s[i]-'a']++
+	}
+	for i := 0; i < len(s); i++ {
+		if bucket[s[i]-'a'] == 1 {
+			return i
 		}
 	}
-	min := math.MaxInt32
-	for _, charIndex := range bucket {
-		if charIndex > 0 && charIndex-1 < min {
-			min = charIndex - 1
-		}
-	}
-	if min == math.MaxInt32 {
-		return -1
-	}
-	return min
+	return -1
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
