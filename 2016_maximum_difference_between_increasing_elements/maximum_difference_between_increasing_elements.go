6ff53a193fe6db3296c39c59e3489d2a8497ccc6
mutoe <mutoe@foxmail.com>
2021-10-01 22:19:25 +0800

    2016 0ms 100%

--- 2016_maximum_difference_between_increasing_elements/maximum_difference_between_increasing_elements.go
+++ 2016_maximum_difference_between_increasing_elements/maximum_difference_between_increasing_elements.go
@@ -3,28 +3,24 @@ package maximum_difference_between_increasing_elements
 // https://leetcode.com/problems/maximum-difference-between-increasing-elements
 
 // level: 1
-// time: O(n) 8ms 18.57%
-// space: O(1) 2.8M 28.57%
+// time: O(n) 0ms 100%
+// space: O(1) 2.6M 37.14%
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func maximumDifference(nums []int) int {
-	n := len(nums)
-	max := -1
-	i, j := 0, 1
-	for i < n && j < n {
-		if nums[j] < nums[i] {
-			i, j = j, j+1
-			continue
+	diff := -1
+	min, max := nums[0], nums[0]
+	for _, num := range nums {
+		if num < min {
+			min, max = num, num
+		} else if num > max {
+			max = num
+			if d := max - min; d > diff {
+				diff = d
+			}
 		}
-		if diff := nums[j] - nums[i]; diff > max {
-			max = diff
-		}
-		j++
 	}
-	if max == 0 {
-		return -1
-	}
-	return max
+	return diff
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
