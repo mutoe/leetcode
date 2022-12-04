15c6b0f584680f9671cfbf142f8db5e2123ee0a3
mutoe <imutoe@gmail.com>
2022-12-04 12:49:27 +0800

    119 1ms 59.83%

--- 119_pascals_triangle_ii/pascals_triangle_ii.go
+++ 119_pascals_triangle_ii/pascals_triangle_ii.go
@@ -3,7 +3,7 @@ package pascals_triangle_ii
 // https://leetcode.com/problems/pascals-triangle-ii
 
 // level: 1
-// time: O(n^2) 3ms 21.07%
+// time: O(n^2) 1ms 59.83%
 // space: O(n^2) 2.1M 39.89%
 
 // leetcode submit region begin(Prohibit modification and deletion)
@@ -12,12 +12,13 @@ func getRow(rowIndex int) []int {
 	dp[0] = []int{1}
 	for i := 1; i <= rowIndex; i++ {
 		dp[i] = make([]int, i+1)
-		for j := 0; j <= i; j++ {
-			if j == 0 || j == i {
-				dp[i][j] = 1
+		for j := 0; j <= i/2; j++ {
+			if j == 0 {
+				dp[i][j], dp[i][i-j] = 1, 1
 				continue
 			}
-			dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+			r := dp[i-1][j-1] + dp[i-1][j]
+			dp[i][j], dp[i][i-j] = r, r
 		}
 	}
 	return dp[rowIndex]
