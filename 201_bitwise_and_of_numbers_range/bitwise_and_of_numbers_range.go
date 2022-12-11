98ac4a8b7cbe7607084552731542b09aacfd707e
mutoe <imutoe@gmail.com>
2022-12-11 15:43:20 +0800

    201 626ms 6.25%

--- 201_bitwise_and_of_numbers_range/bitwise_and_of_numbers_range.go
+++ 201_bitwise_and_of_numbers_range/bitwise_and_of_numbers_range.go
@@ -3,13 +3,13 @@ package bitwise_and_of_numbers_range
 // https://leetcode.com/problems/bitwise-and-of-numbers-range
 
 // level: 2
-// time: O(n) 822ms 6.25%
-// space: O(1) 5.3M 18.75%
+// time: O(n) 626ms 6.25%
+// space: O(1) 5.2M 91.67%
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func rangeBitwiseAnd(left int, right int) int {
 	result := left
-	for i := left + 1; i <= right; i++ {
+	for i := right; i > left; i-- {
 		result &= i
 		if result == 0 {
 			return 0
